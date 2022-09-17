6df9fdcf4a57f6ed6b5c4df950c5ba657864b21d
Sadik Ahammed Shaik <sadikahammed1996@gmail.com>
2022-09-17 17:26:26 +0530

    Updating readme file

--- App.js
+++ App.js
@@ -2,11 +2,16 @@ import http from 'http';
 import { fork } from 'child_process';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import cors from 'cors'
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 http
   .createServer(async (req, res) => {
+    cors({
+      origin: '*',
+      methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+    })
     const child = fork(__dirname + '/ChildProcess.js');
     child.on('message', (data) => {
         res.setHeader('Content-Type', 'application/json');
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # NodeJS_ADP
+
+# Steps to run the app
+1.Make sure your node_version is 14.
+2.npm install
+3.npm start - will run node App.js
+4.npm run nodemon - will run nodemon App.js
