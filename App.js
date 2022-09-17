677f89ca637d636aa0fefa52d75a21fecee078e5
Sadik Ahammed Shaik <sadikahammed1996@gmail.com>
2022-09-17 17:55:37 +0530

    Adding comments

--- App.js
+++ App.js
@@ -6,13 +6,17 @@ import cors from 'cors'
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+//Creating and listening to the server
 http
   .createServer(async (req, res) => {
+    //Accepting all the origins and https methods to avoid CORS issues
     cors({
       origin: '*',
       methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
     })
-    const child = fork(__dirname + '/ChildProcess.js');
+    const child = fork(__dirname + '/ChildProcess.js'); // Doing child_process using fork in ChildProcess.js
+
+    //listening to the message from the process which was sent in ChildProcess.js.
     child.on('message', (data) => {
         res.setHeader('Content-Type', 'application/json');
         res.write(data);
--- ChildProcess.js
+++ ChildProcess.js
@@ -1,5 +1,6 @@
 import fetch from 'node-fetch';
 
+//Doing a get call and fetching the data
 const data = await fetch('https://interview.adpeai.com/api/v2/get-task');
 const json = await data.json();
 const nameAndAmoutMap = new Map();
@@ -7,8 +8,10 @@ let name = '';
 let amount = 0;
 let alphaIds;
 
+//looping all the transctions from the response
 for (const iterator of json.transactions) {
     const date = new Date(iterator.timeStamp);
+  //Setting the map with employee name and salar, alpha ids as key value pairs
   if (date.getFullYear() === 2021) {
     if (nameAndAmoutMap.get(iterator.employee.name)) {
       const data = nameAndAmoutMap.get(iterator.employee.name);
@@ -29,6 +32,7 @@ for (const iterator of json.transactions) {
 
 const entries = [...nameAndAmoutMap.entries()];
 
+//looping all the entries in the map and extracting name and alphaIds of highest earner of the year
 for (const [key, value] of entries) {
   if(value.salary > amount) {
       name = key;
@@ -37,6 +41,7 @@ for (const [key, value] of entries) {
   }
 }
 
+//Doing a post call with Id and alphaIds as body
 const postResponse = await fetch('https://interview.adpeai.com/api/v2/submit-task', {
     method: 'post',
     body: JSON.stringify({
@@ -45,6 +50,7 @@ const postResponse = await fetch('https://interview.adpeai.com/api/v2/submit-tas
     })
 })
 
+//Sending the message with the process
 process.send(
   `${name} is the highest earner of the year 2021 with an amount of ${amount} and the alpha transaction Ids of ${name} are:
   ${alphaIds}
